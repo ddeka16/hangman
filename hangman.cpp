13b81077c6d58755a2dcf1af7957342f90dcda86
ddeka16 <danieldeka@interia.pl>
2025-04-17 15:32:01 +0200

    noapiyet

--- hangman.cpp
+++ hangman.cpp
@@ -4,12 +4,13 @@
 using namespace std;
 class Hangman {
 public:
-	int lives = 3;
+	int lives = 5;
 	string word;
 	string guessedletter;
 	char guess(string word, int& lives);
 	void viewWord(string word);
 	void check(string word);
+	void draw(int lives);
 	bool win = false;
 };
 
@@ -17,13 +18,14 @@ char Hangman::guess(string word, int &lives) {
 	char letter;
 	bool right = false;
 	string input;
+	cout << endl;
 	cout << "Zgadnij litere: ";
 	while (true) {
 		getline(cin, input);
 		if (input.length() == 0) {
 			
-				cout << "Nie wprowadziles zadnego znaku. Sprobuj ponownie" << endl;
-			}
+			cout << "Nie wprowadziles zadnego znaku. Sprobuj ponownie" << endl;
+		}
 		else if (input.length() == 1) {
 			letter = input[0];
 			if (guessedletter.find(letter) != string::npos) {
@@ -36,20 +38,17 @@ char Hangman::guess(string word, int &lives) {
 		else if (input.length() > 1) {
 			cout << "Wpisales za duzo liter. Sprobuj wpisac jedna litere ponownie" << endl;
 		}
-		
-		
 	}
 	for (int i = 0; i < word.length(); i++) {
 		if (letter == word[i]) {
-			cout << "Brawo! Trafiles litere" << endl;
 			guessedletter += letter;
 			return letter;
 			right = true;
 		}
 	}
 	if (right == false) {
+		guessedletter += letter;
 		lives -= 1;
-			cout << "Nie trafiles. Zostalo ci " << lives << " zyc." << endl;
 	}
 }
 
@@ -76,19 +75,67 @@ void Hangman::check(string word) {
 			return;
 			}
 	}
-	cout << word << endl;
+	cout << "Haslo to: " << word << endl;
 	cout << "Gratulacje! Wygrywasz!!!" << endl;
 	win = true;
 }
 
+void Hangman::draw(int lives) {
+	if (lives == 0) {
+		cout << "Pozostale zycia: " << lives << endl;
+		cout << "----------------" << endl;
+		cout << "     |          " << endl;
+		cout << "     0     " << endl;
+		cout << "    /|\\     " << endl;
+		cout << "     |    " << endl;
+		cout << "    / \\    " << endl;
+		cout << "Niestety, przegrales :(" << endl;
+	}
+	else if (lives == 1) {
+		cout << "Pozostale zycia: " << lives << endl;
+		cout << "----------------" << endl;
+		cout << "     |          " << endl;
+		cout << "     0     " << endl;
+		cout << "    /|\\     " << endl;
+		cout << "     |    " << endl;
+	}
+	else if (lives == 2) {
+		cout << "Pozostale zycia: " << lives << endl;
+		cout << "----------------" << endl;
+		cout << "     |          " << endl;
+		cout << "     0     " << endl;
+		cout << "    /|\\     " << endl;
+	}
+	else if (lives == 3) {
+		cout << "Pozostale zycia: " << lives << endl;
+		cout << "----------------" << endl;
+		cout << "     |          " << endl;
+		cout << "     0     " << endl;
+
+	}
+	else if (lives == 4) {
+		cout << "Pozostale zycia: " << lives << endl;
+		cout << "----------------" << endl;
+		cout << "     |          " << endl;
+	}
+	else if (lives == 5) {
+		cout << "Pozostale zycia: " << lives << endl;
+		cout << "----------------" << endl;
+	}
+}
 
 int main() {
 	Hangman h;
 	getline(cin, h.word);
-	cout << "Masz " << h.lives << "zyc" << endl;
 	while (h.lives > 0 && h.win == false) {
+		system("cls");
+		h.draw(h.lives);
 		h.viewWord(h.word);
 		h.guess(h.word, h.lives);
-		h.check(h.word);
+		h.check(h.word);		
+	}
+	if (h.lives == 0 && h.win == false) {
+		system("cls");
+		h.draw(h.lives);
 	}
 }
